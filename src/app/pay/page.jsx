073531d97fc1d1a04e5d0b88047f1c99b8c1e786
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-10-15 16:40:22 +0530

    add payments page

---
+++ src/app/pay/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+function page() {
+  const [address, setAddress] = useState();
+  const [token, setToken] = useState();
+  return <div>payments page</div>;
+}
+export default page;
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -4,18 +4,29 @@ import {
   signInWithPopup,
   signOut,
 } from "firebase/auth";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useEffect } from "react";
 function Auth() {
+  const router = useRouter();
   const signInWithGoogle = async () => {
     const res = await signInWithPopup(auth, googleAuthProvider);
     console.log(res);
+    if (auth?.currentUser?.email) {
+      router.push("/pay");
+    }
   };
   const logout = async () => {
     const res = await signOut(auth);
     console.log(res);
   };
-  console.log(auth?.currentUser?.email);
+  console.log(auth);
+
+  useEffect(() => {
+    if (auth?.currentUser?.email) {
+      router.push("/pay");
+    }
+  });
 
   return (
     <div>
